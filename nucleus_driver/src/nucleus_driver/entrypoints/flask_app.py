241ec00d4009829d0fb4a1a6b0a8c45c565cb20a
Martin <martin.johansen@nortekgroup.com>
2023-01-17 16:06:17 +0100

    tweaking

--- nucleus_driver/src/nucleus_driver/entrypoints/flask_app.py
+++ nucleus_driver/src/nucleus_driver/entrypoints/flask_app.py
@@ -218,10 +218,10 @@ def mavlink_get_specific_param():
 
     #response = get_parameter("AHRS_EKF_TYPE")
     response = requests.get(MAVLINK2REST_URL + "/mavlink/vehicles/1/components/1/messages/AHRS_EKF_TYPE") #, json=data)
-    logging.info(f'AHRS_EKF_TYPE: {json.loads(response).text}')
-    
+    logging.info(f'AHRS_EKF_TYPE: {response.json()}')
 
-    return response
+
+    return response.json()
 
 class RovLink:
 
