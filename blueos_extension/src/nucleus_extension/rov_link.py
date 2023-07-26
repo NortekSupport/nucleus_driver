050567cab7fcc5184bc6b7856d7b1cf1376623ef
Martin Bergene Johansen <martin.johansen@nortekgroup.com>
2023-07-26 15:44:00 +0200

    updated parameter reading

--- blueos_extension/src/nucleus_extension/rov_link.py
+++ blueos_extension/src/nucleus_extension/rov_link.py
@@ -251,7 +251,8 @@ class RovLink(Thread):
 
         
         session = requests.Session()
-        retry = Retry(total=retries, backoff_factor=1, status_forcelist=[502])
+        #retry = Retry(total=retries, backoff_factor=1, status_forcelist=[502])
+        retry = Retry(total=retries, backoff_factor=1)
         adapter = HTTPAdapter(max_retries=retry)
         session.mount('http://', adapter)
         
