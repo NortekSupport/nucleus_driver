80dda2698eee19c045e5c315884f5591d7b023fc
Martin Bergene Johansen <martin.johansen@nortekgroup.com>
2024-09-25 15:12:14 +0200

    fixed current profile format for velocity data in ros2 node

--- ros2/src/nucleus_driver_ros2/setup.py
+++ ros2/src/nucleus_driver_ros2/setup.py
@@ -4,7 +4,7 @@ package_name = "nucleus_driver_ros2"
 
 setup(
     name=package_name,
-    version="0.2.0",
+    version="0.2.1",
     packages=["nucleus_node", "nucleus_clients", "nucleus_subscribers"],
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
